7b159262b0d4257f323643f942fa01dc93df382f
lujing <lujing@zhixuezhen.com>
2019-04-10 11:09:31 +0800

    fix: 按需引入elementui

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Element from './plugins/element-ui' // 按需加载
+import './plugins/element-ui' // 按需加载
 import App from './App'
 import router from './router'
 
-Vue.use(Element)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
